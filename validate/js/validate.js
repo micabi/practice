fd9a98227e01db67e529365cc2f1561eac0b2295
Wataru <w.adachi1127@gmail.com>
2017-01-09 17:01:30 +0900

    更新　関数化
    でも、stackoverflow。直さなきゃ。

--- validate/js/validate.js
+++ validate/js/validate.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
 
 	// お名前
 	$('[name=name]').on('blur', function(){
-		/*
 		var alertName = "お名前は入力必須項目です。";
 		var yourName = $(this).val();
 		if (yourName === "") {
@@ -10,8 +9,6 @@ $(document).ready(function(){
 		}else{
 			$(this).prev('span.alert').text("");
 		}
-		*/
-		nameCheck();
 	});
 
 	// フリガナ
@@ -89,10 +86,10 @@ $(document).ready(function(){
 		var yourSex = $('[name=sex]:checked').val();
 		if (yourSex === undefined) {
 			$(this).prev('span.alert').text(alertSex);
-			console.log("undifined");
+			//console.log("undifined");
 		}else{
 			$(this).prev('span.alert').text("");
-			console.log(yourSex);
+			//console.log(yourSex);
 		}
 	});
 
@@ -129,10 +126,10 @@ $(document).ready(function(){
 		});
 
 		$('[name=comment]').on('blur', function(){
+
 			$('p.length').css({
 				'color': '#000'
 			});
-
 			var alertComment = "コメントは入力必須項目です。";
 				if ($(this).val() === "") {
 					$(this).prev('span.alert').text(alertComment);
@@ -145,14 +142,22 @@ $(document).ready(function(){
 				}
 		});
 
-		$('form#form').on('submit', function(){
-			//console.log(nameCheck);
+		$('form#form').on('submit', function(event){
+			event.preventDefault();
+
+
+			if(!nameCheck || !kanaCheck || !telCheck || !mailCheck || !zipCheck || !sexCheck || !oldCheck || !commentCheck){
+				alert("未記入の項目、または形式が適切でない項目があります。");
+				$(this).off('submit');
+				return false;
+			}
+
+			$('form#form').submit();
 		});
 
-		function formCheck(){
-			nameCheck();
-			return false;
-		}
+/******************************
+関数定義
+******************************/
 
 		function nameCheck(){
 			var alertName = "お名前は入力必須項目です。";
@@ -162,35 +167,128 @@ $(document).ready(function(){
 				return false;
 			}else{
 				$('[name=name]').prev('span.alert').text("");
-				return true;
+
 			}
 		}
 
 		function kanaCheck(){
+				var alertKana = "全角カタカナで記入して下さい。";
+				var yourKana = $('[name=kana]').val();
+				yourKana = yourKana.replace(/[\n\s ]/g, '');
+				//console.log(yourKana.length);
 
-		}
+				if (yourKana === "") {
+					$('[name=kana]').prev('span.alert').text("記入して下さい。");
+					return false;
+				}else if(!yourKana.match(/^[ァ-ン]*$/)) {
+					$('[name=kana]').prev('span.alert').text(alertKana);
+					return false;
+				}else{
+					$('[name=kana]').prev('span.alert').text("");
 
-		function mailCheck(){
-
-		}
+				}
+			}
 
 		function telCheck(){
+				var alertTel = "電話番号は10桁または11桁の数字で記入して下さい。";
+				var yourTel = $('[name="tel"]').val();
+				yourTel = yourTel.replace(/[\n\s ]/g, '');
+				yourTel = yourTel.replace(/[━.*‐.*―.*－.*\–.*ー.*\-]/gi,'');
+				var formatTel = yourTel.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s){return String.fromCharCode(s.charCodeAt(0)-0xFEE0);});
+				formatTel = $.trim(formatTel);
+				//console.log(formatTel);　/[Ａ-Ｚａ-ｚ０-９]/g
+				//console.log(formatTel.length);
 
-		}
+				if (formatTel.length < 10 || formatTel.length > 11 || formatTel === "") {
+					$('[name="tel"]').prev('span.alert').text(alertTel);
+					return false;
+				}else{
+					$('[name="tel"]').val(formatTel);
+					$('[name="tel"]').prev('span.alert').text("");
+
+				}
+			}
+
+		function mailCheck(){
+				var alertMail = "メールアドレスの形式ではありません。入力必須項目です。";
+				var yourMail = $('[name=mail]').val();
+				yourMail = yourMail.replace(/[\n\s ]/g, '');
+				//console.log(yourMail);
+
+				if (!yourMail.match(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/)) {
+					$('[name=mail]').prev('span.alert').text(alertMail);
+					return false;
+				}else{
+					$('[name=mail]').prev('span.alert').text("");
+					//console.log(yourMail);
+
+				}
+			}
 
 		function zipCheck(){
+				var alertZip = "郵便番号は7桁の数字で記入して下さい。";
+				var yourZip = $('[name=zip]').val();
+				yourZip = yourZip.replace(/[━.*‐.*―.*－.*\–.*ー.*\-]/gi,'');
+				yourZip = yourZip.replace(/[\n\s ]/g, ''); // スペース、改行を除去
+				//console.log(yourZip);
+				var formatZip = yourZip.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s){return String.fromCharCode(s.charCodeAt(0)-0xFEE0);});
+				//console.log(formatZip);
+				//console.log(formatZip.length);
 
-		}
+				if (formatZip.length != 7) {
+					$('[name=zip]').prev('span.alert').text(alertZip);
+					return false;
+				}else{
+					$('[name=zip]').val(formatZip);
+					$('[name=zip]').prev('span.alert').text("");
+
+				}
+			}
 
 		function sexCheck(){
+				var alertSex = "1つ選択して下さい。";
+				var yourSex = $('[name=sex]:checked').val();
+				if (yourSex === undefined) {
+					$('.alertsex').text(alertSex);
+					return false;
+				}else{
+					$('.alertsex').text("");
+					//console.log(yourSex);
 
-		}
+				}
+			}
 
 		function oldCheck(){
+				var alertOld = "1つ選択して下さい。";
+				var yourOld = $('[name=old]').val();
+				if (yourOld === null) {
+					$('[name=old]').prev('span.alert').text(alertOld);
+					//console.log("null");
+					return false;
+				}else if(yourOld === ""){
+					$('[name=old]').prev('span.alert').text(alertOld);
+					//console.log("空");
+					return false;
+				}else{
+					$('[name=old]').prev('span.alert').text("");
+					//console.log(yourOld);
 
-		}
+				}
+			}
 
 		function commentCheck(){
+				var alertComment = "コメントは入力必須項目です。";
+					if ($('[name=comment]').val() === "") {
+						$('[name=comment]').prev('span.alert').text(alertComment);
+						return false;
+					}else{
+						$('[name=comment]').prev('span.alert').text("");
 
-		}
+					}
+
+					if ($('[name=comment]').val().length > 50) {
+						$('[name=comment]').prev('span.alert').text("50文字以内で記入して下さい。");
+						return false;
+					}
+			}
 });
