7c712fe5621816e1be5c99cd9ae12249c02e6dd7
Wataru <w.adachi1127@gmail.com>
2017-04-01 01:02:46 +0900

    更新　完成！ e.targetを覚えた

--- object_index/index.js
+++ object_index/index.js
@@ -9,6 +9,27 @@ $(document).ready(function(){
 	});
 */
 
+function Change(element1, element2){
+	this.getDocuments(element1, element2);
+}
 
+Change.prototype.getDocuments = function(element1, element2){
+	this.$tab = element1;
+	this.$panel = element2;
+	this.display();
+};
+
+Change.prototype.display = function(){
+	var tab = this.$tab;
+	var panel = this.$panel;
+	tab.on('click', function(e){
+		var index = $(e.target).parent('li').index();
+		panel.removeClass('show');
+		panel.eq(index).addClass('show');
+	});
+
+};
+
+var tab = new Change($('ul.tab').find('li').children('a'), $('div.panel'));
 
 });
