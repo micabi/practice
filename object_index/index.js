0a621123817322ed6cf177d14d416aae46415901
Wataru <w.adachi1127@gmail.com>
2017-03-31 23:39:54 +0900

    新規　タブ切り替えをオブジェクト指向で挑戦

---
+++ object_index/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+/*
+	$('ul.tab').find('li').children('a').on('click', function(e){
+		e.preventDefault();
+		var $index = $(this).parent('li').index();
+
+		$('div.panel').removeClass('show');
+		$('div.panel').eq($index).addClass('show');
+	});
+*/
+
+
+
+});
