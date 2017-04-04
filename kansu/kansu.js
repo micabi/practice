72e94e1702e1282ce1fcaf01e4117999be9e2763
WATARU <w.adachi1127@gmail.com>
2017-04-04 19:30:29 +0900

    新規　kansu

---
+++ kansu/kansu.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+
+	var val = "Global";
+
+	function getVal(){
+		console.log(val); // undefined
+		var val = "Local";
+		return val;
+	}
+
+	console.log(getVal()); // Local
+	console.log(val); // Global
+
+};
